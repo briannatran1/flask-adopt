f5ee8a30f9600622b69ca574340f05d63e37c04d
Brianna Tran <briannatran45@gmail.com>
2023-09-01 16:46:57 -0700

    no change

--- forms.py
+++ forms.py
@@ -1,17 +1,17 @@
 """Forms for adopt app."""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, IntegerField, TextAreaField, SelectField
+from wtforms import StringField, TextAreaField
 from wtforms.validators import InputRequired, Optional, URL, AnyOf
 
 
-class AddPetForm(FlaskForm):
+class PetForm(FlaskForm):
     """Form for adding pets."""
 
     name = StringField('Pet name', validators=[InputRequired()])
-    species = StringField('Species', validators=[InputRequired(), AnyOf(['cat', 'dog', 'porcupine'], message='Species must be cat, dog, or porcupine')])
+    species = StringField('Species', validators=[InputRequired(), AnyOf(
+        ['cat', 'dog', 'porcupine'], message='Species must be cat, dog, or porcupine')])
     photo_url = StringField('Photo URL', validators=[Optional(), URL()])
-    age = StringField('Age', validators=[InputRequired(), AnyOf(['baby', 'young', 'adult', 'senior'], message='Age must be baby, young, adult, or senior')])
+    age = StringField('Age', validators=[InputRequired(), AnyOf(
+        ['baby', 'young', 'adult', 'senior'], message='Age must be baby, young, adult, or senior')])
     notes = TextAreaField('Notes', validators=[Optional()])
-
-                      #choices=[('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'), ('senior', 'Senior')])
